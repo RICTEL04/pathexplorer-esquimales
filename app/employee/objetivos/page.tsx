e5b1b17781b6acf43a0c99d563d5560ce88b9c74
void-david <void9david@gmail.com>
2025-04-07 17:31:26 -0600

    added folders for employee sidebar paths

---
+++ app/employee/objetivos/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function ObjetivosPage() {
+    return (
+        <div>
+          <h1 className="text-gray-800 text-2xl font-bold mb-4">Objetivos</h1>
+          <p className="text-gray-600">Aquí puedes gestionar tus objetivos.</p>
+          {/* Add your content for Objetivos here */}
+        </div>
+    );
+}
