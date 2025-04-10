eedd0ef61f9566e77007b1e4361f03c031528595
RamonAntonioNaranjoSarmient <a00836530@tec.mx>
2025-04-10 14:37:01 -0600

    cambio en columna 2 lol
    nomas cambié el span a 10  para que el background de perfil se vea mejor suptm arriiba las pinches aguilas del america carajo

--- app/capability-lead/perfil/[id]/page.tsx
+++ app/capability-lead/perfil/[id]/page.tsx
@@ -79,7 +79,7 @@ const UserProfilePage = ({ params }: { params: { id: string } }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 w-full">
 
         {/* Columna central */}
-        <div className="lg:col-span-8 space-y-6">
+        <div className="lg:col-span-10 space-y-6">
           <Profile {...userProfileData} />
         </div>
 
--- app/employee/perfil/page.tsx
+++ app/employee/perfil/page.tsx
@@ -78,7 +78,7 @@ const UserProfilePage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 w-full">
 
         {/* Columna central */}
-        <div className="lg:col-span-8 space-y-6">
+        <div className="lg:col-span-10 space-y-6">
           <Profile {...userProfileData} />
         </div>
 
