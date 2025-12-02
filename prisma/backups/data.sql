bd42c0e22a13077d7a126ce5b23a5cd950ddb66e
void-david <void-david@users.noreply.github.com>
2025-12-02 00:22:11 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 5eqTswDSKTc8RIeoph1FiGuszi5DMpgi7XfhhsCVXXUdkaukeWlaMRQb6DSdFtt
+-- \restrict nFfpxgMfXEOUgIlhmIzXUb1SCnWutsdQlibZb4CwiasSgqsEE47XbOsdyAF0Wgh
 
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.6
@@ -6787,6 +6787,6 @@ SELECT pg_catalog.setval('"public"."Notificaciones_id_seq"', 2, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 5eqTswDSKTc8RIeoph1FiGuszi5DMpgi7XfhhsCVXXUdkaukeWlaMRQb6DSdFtt
+-- \unrestrict nFfpxgMfXEOUgIlhmIzXUb1SCnWutsdQlibZb4CwiasSgqsEE47XbOsdyAF0Wgh
 
 RESET ALL;
